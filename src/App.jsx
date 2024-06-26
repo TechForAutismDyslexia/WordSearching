f691c26cf4f038a48e19a4f5d5c1fa380f6d7142
Jhansi <jchandaluri@gmail.com>
2024-06-26 21:05:19 +0530

    levelsAdded

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import PixiGame4 from './PixiGame4'
 import PixiGame5 from'./PixiGame5'
 import PixiGame6 from './PixiGame6'
 import PixiGame7 from './PixiGame7'
+import PixiGame8 from './PixiGame8'
+import PixiGame9 from './PixiGame9'
+import PixiGame10 from './PixiGame10'
 function App() {
 
   return (
@@ -23,6 +26,9 @@ function App() {
         <Route exact path='/game5' element = {<PixiGame5/>}/>
         <Route exact path='/game6' element = {<PixiGame6/>}/>
         <Route exact path='/game7' element = {<PixiGame7/>}/>
+        <Route exact path='/game8' element = {<PixiGame8/>}/>
+        <Route exact path='/game9' element = {<PixiGame9/>}/>
+        <Route exact path='/game10' element = {<PixiGame10/>}/>
         {/* <Route exact path='/game4' element={<PixiComponent4/>}/> */}
       </Routes>
     </Path>
