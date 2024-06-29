b8bd385d6a2ac85e79e0059a3726297a5105b2d9
AmazingCuber <saivenkatkrishna2004@gmail.com>
2024-06-29 16:18:11 +0530

    Mobile time

--- src/PixiGame6.jsx
+++ src/PixiGame6.jsx
@@ -511,7 +511,7 @@ else{
       <div className="image-container">
         <img src='./info_pic.png' alt="Descriptive Image" className="hover-image" onClick={()=>readOutLoud("Find the words listed below  Click and drag on the letters to select them")} style={{height: 35}}/>
         <span style={{display: 'flex'}}><div className="description">Find the words listed below  Click and drag on the letters to select them.</div>
-        <h4 style={{marginLeft: window.innerWidth/12}}>Tries: {tries}</h4></span>
+        <h5 style={{marginLeft: window.innerWidth/15, padding: 0, width: 100}}>Time: {ongoingElapsedTime.toFixed(0)}</h5><h4 style={{marginLeft: window.innerWidth/12, padding: 0, width: 100}}>Tries: {tries}</h4></span>
       </div>
     </div>
         {/* <Voice ReadingText={"Find the words listed below  Click and drag on the letters to select them"}/> */}
