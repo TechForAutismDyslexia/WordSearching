3fb888244ae9351d3eb3078d507ad9402829a9e0
AmazingCuber <saivenkatkrishna2004@gmail.com>
2024-07-07 13:43:16 +0530

    exact removed from exact path

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,18 @@ function App() {
   return (
     <Router basename="/games/wordsearching">
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/game1" element={<PixiGame1 />} />
-        <Route exact path="/game2" element={<PixiGame2 />} />
-        <Route exact path="/game3" element={<PixiGame3 />} />
-        <Route exact path="/game4" element={<PixiGame4 />} />
-        <Route exact path="/game5" element={<PixiGame5 />} />
-        <Route exact path="/game6" element={<PixiGame6 />} />
-        <Route exact path="/game7" element={<PixiGame7 />} />
-        <Route exact path="/game8" element={<PixiGame8 />} />
-        <Route exact path="/game9" element={<PixiGame9 />} />
-        <Route exact path="/game10" element={<PixiGame10 />} />
-        <Route exact path="/Completed" element={<Completed />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/game1" element={<PixiGame1 />} />
+        <Route path="/game2" element={<PixiGame2 />} />
+        <Route path="/game3" element={<PixiGame3 />} />
+        <Route path="/game4" element={<PixiGame4 />} />
+        <Route path="/game5" element={<PixiGame5 />} />
+        <Route path="/game6" element={<PixiGame6 />} />
+        <Route path="/game7" element={<PixiGame7 />} />
+        <Route path="/game8" element={<PixiGame8 />} />
+        <Route path="/game9" element={<PixiGame9 />} />
+        <Route path="/game10" element={<PixiGame10 />} />
+        <Route path="/Completed" element={<Completed />} />
       </Routes>
     </Router>
   );
